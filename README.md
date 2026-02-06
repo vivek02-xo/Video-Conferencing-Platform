1b643bf4c48ac2318cb68e9dd5c883518611afca
vivek02-xo <vivek02.xo@gmail.com>
2026-02-06 12:24:28 +0530

    Frontend setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Video-Conferencing-Platform
 A full-stack real-time video conferencing application featuring secure authentication, peer-to-peer video/audio calls, live chat, meeting rooms, and scalable architecture. Built to understand real-time communication systems, WebRTC, and modern backend/frontend integration.
+
---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/Landing';
+import Authentication from './pages/Authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/Home';
+import History from './pages/History';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Router>
+
+        <AuthProvider>
+
+
+          <Routes>
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
